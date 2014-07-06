b1c6d062ec5fd5cd9e477e345759cbb16eaf0efb
Jainy Zau <jainyzau@gmail.com>
2014-07-06 06:10:30 -0400

    Initial files

---
+++ mytime/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mytime::Application.config.secret_key_base = 'b8765dd3b4bfc58418c2ad734f148a64a3e0aaf1f59390271dab98df7b83d530516c7d68c580b65fa13e3479a77c38fb0d9ad2aa18c6d1946cf9506f6d43dfc2'
